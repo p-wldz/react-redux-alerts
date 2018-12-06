7e2ec5af35eb84142e378c7ee7b649ec4d644656
Your Name <wlodziuss@wp.pl>
2018-12-06 16:31:20 +0100

    Added form for adding messages

--- src/components/AlertSimpleManager.js
+++ src/components/AlertSimpleManager.js
@@ -3,16 +3,30 @@ import React, { Component } from 'react';
 export default class AlertSimpleManager extends Component {
     constructor(props) {
         super(props);
-        this.addSuccessMessage.bind(this);
+        this.state = {
+            message: ""
+        };
+        this.addSuccessMessage = this.addSuccessMessage.bind(this);
+        this.changeValues = this.changeValues.bind(this);
     }
-    addSuccessMessage()
+    addSuccessMessage(e)
     {
+        e.preventDefault();
         console.log("Test");
+    }
+    changeValues(e)
+    {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
     }
     render(){
         return (
             <div>
-                <button onClick={this.addSuccessMessage} type="button" className="btn btn-primary">Add new success message</button>
+                <form onSubmit={this.addSuccessMessage}>
+                    <input type="text" onChange={this.changeValues} name="message" value={this.state.message} />
+                    <button type="submit" className="btn btn-primary">Add new success message</button>
+                </form>
             </div>
         );
     }
