30275ec920464e3f39091b938579301bd4a0abc4
Your Name <wlodziuss@wp.pl>
2018-12-07 18:59:07 +0100

    Changed state to props in AlertList component

--- src/components/AlertList.js
+++ src/components/AlertList.js
@@ -4,26 +4,16 @@ import Alert from './Alert';
 export default class AlertList extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            alerts: this.props.alerts
-        }
-    }
-    componentWillReceiveProps({alerts}) {
-        this.setState(
-            {
-                alerts
-            }
-        );
     }
     render(){
-        if (this.state.alerts.length == 0)
+        if (this.props.alerts.length == 0)
         {
             return <div><i>Please, add new message..</i></div>
         }
         return (
             <div>
                 <div className="bd-example">
-                    {this.state.alerts.map((alert, index) => (
+                    {this.props.alerts.map((alert, index) => (
                             <Alert key={index} alert={alert}/>
                         )
                     )}
